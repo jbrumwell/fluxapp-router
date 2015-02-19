74b739221c9967006a6db4efcac791f9119cfb27
Michał Kawalec <michal@bazzle.me>
2015-02-19 17:10:01 +0100

    the current class is set correctly

--- lib/components/Link.jsx
+++ lib/components/Link.jsx
@@ -10,18 +10,48 @@ var a = React.DOM.a;
 
 module.exports = React.createClass({
 
+  mixins : [ router._getFluxApp().mixins.component ],
+
+  flux : {
+    stores : {
+      updateCurrentState : router.getStore()
+    }
+  },
+
+  updateCurrentState : function updateCurrentState() {
+    var store = router.getStore();
+    var isCurrent = false;
+
+    if (this.props.href === store.state.current.path) {
+      isCurrent = true;
+    }
+
+    this.setState({
+      isCurrentRoute : isCurrent
+    });
+  },
+
   onClick : function(e) {
     e.preventDefault();
 
     router.go(this.props.href, { force : this.props.force });
   },
 
+  getInitialState : function getInitialState() {
+    return {
+      isCurrentRoute : false
+    };
+  },
+
+  componentDidMount : function componentDidMount() {
+    this.updateCurrentState();
+  },
+
   render : function() {
     var props = _.clone(this.props);
     delete props.force;
 
-    var store = router.getStore();
-    if (props.href === store.state.current.path) {
+    if (this.state.isCurrentRoute) {
       props.className = 'current';
     }
 
