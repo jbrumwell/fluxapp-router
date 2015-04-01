70fddfd457402fe01a56b2a6eedebaf25ba06b0f
Michal Kawalec <michal@bazzle.me>
2015-04-01 14:22:37 +0200

    architecture rewrite

--- lib/components/Form.jsx
+++ lib/components/Form.jsx
@@ -2,34 +2,35 @@
 'use strict';
 
 var React = require('react/addons');
-var router = require('../index');
 var _ = require('lodash');
 
 var form = React.DOM.form;
 
 
-module.exports = React.createClass({
+module.exports = function(router) {
+  return React.createClass({
 
-  onSubmit : function handleSubmit(e) {
-    e.preventDefault();
+    onSubmit : function handleSubmit(e) {
+      e.preventDefault();
 
-    router.go(this.props.action, { 
-      method : this.props.method, 
-      force  : this.props.force,
-      refs   : this.refs
-    });
-  },
+      router.go(this.props.action, {
+        method : this.props.method,
+        force  : this.props.force,
+        refs   : this.refs
+      });
+    },
 
-  render : function render() {
-    var props = _.clone(this.props);
-    delete props.force;
+    render : function render() {
+      var props = _.clone(this.props);
+      delete props.force;
 
-    if (router.isEnabled()) {
-      props.onSubmit = props.onSubmit || this.onSubmit;
-    } else {
-      delete props.onSubmit;
+      if (router.isEnabled()) {
+        props.onSubmit = props.onSubmit || this.onSubmit;
+      } else {
+        delete props.onSubmit;
+      }
+
+      return form(props);
     }
-
-    return form(props);
-  }
-});
+  });
+};
--- lib/components/Link.jsx
+++ lib/components/Link.jsx
@@ -2,65 +2,66 @@
 'use strict';
 
 var React = require('react/addons');
-var router = require('../index');
 var _ = require('lodash');
 
 var a = React.DOM.a;
 
 
-module.exports = React.createClass({
+module.exports = function(router) {
+  return React.createClass({
 
-  mixins : [ router._getFluxApp().mixins.component ],
+    mixins : [ router._fluxApp.mixins.component ],
 
-  flux : {
-    stores : {
-      updateCurrentState : router.getStore()
+    flux: {
+      stores: {
+        updateCurrentState: router.getStore()
+      }
+    },
+
+    updateCurrentState : function updateCurrentState() {
+      var store = router.getStore();
+      var isCurrent = false;
+
+      if (this.props.href === store.state.current.path) {
+        isCurrent = true;
+      }
+
+      this.setState({
+        isCurrentRoute : isCurrent
+      });
+    },
+
+    onClick : function(e) {
+      e.preventDefault();
+
+      router.go(this.props.href, { force : this.props.force });
+    },
+
+    getInitialState : function getInitialState() {
+      return {
+        isCurrentRoute : false
+      };
+    },
+
+    componentDidMount : function componentDidMount() {
+      this.updateCurrentState();
+    },
+
+    render : function() {
+      var props = _.clone(this.props);
+      delete props.force;
+
+      if (this.state.isCurrentRoute) {
+        props.className = 'current';
+      }
+
+      if (router.isEnabled()) {
+        props.onClick = props.onClick || this.onClick;
+      } else {
+        delete props.onClick;
+      }
+
+      return a(props);
     }
-  },
-
-  updateCurrentState : function updateCurrentState() {
-    var store = router.getStore();
-    var isCurrent = false;
-
-    if (this.props.href === store.state.current.path) {
-      isCurrent = true;
-    }
-
-    this.setState({
-      isCurrentRoute : isCurrent
-    });
-  },
-
-  onClick : function(e) {
-    e.preventDefault();
-
-    router.go(this.props.href, { force : this.props.force });
-  },
-
-  getInitialState : function getInitialState() {
-    return {
-      isCurrentRoute : false
-    };
-  },
-
-  componentDidMount : function componentDidMount() {
-    this.updateCurrentState();
-  },
-
-  render : function() {
-    var props = _.clone(this.props);
-    delete props.force;
-
-    if (this.state.isCurrentRoute) {
-      props.className = 'current';
-    }
-
-    if (router.isEnabled()) {
-      props.onClick = props.onClick || this.onClick;
-    } else {
-      delete props.onClick;
-    }
-
-    return a(props);
-  }
-});
+  });
+};
