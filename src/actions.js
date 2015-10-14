9fee1d265be9668a7c3a14d4c37a0e92cfa51430
Jason Brumwell <jason.brumwell@gmail.com>
2015-10-14 03:14:14 -0400

    Update for fluxapp 0.2.0

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,5 @@
 /* global window */
-import fluxapp from 'fluxapp';
+import fluxapp, { BaseActions } from 'fluxapp';
 
 const router = fluxapp.getRouter();
 
@@ -10,78 +10,80 @@ export default (name, options = { method : 'history' }) => {
            window.history;
   }
 
+  const RouterActions = class extends BaseActions {
+    init(url, meta) {
+      const request = router.build(url, meta, false);
+
+      if (! request) {
+        throw new Error('fluxapp:router:init unable to locate route specified', url);
+      }
+
+      if (isHistoryEnabled()) {
+        window.history.replaceState(
+          request,
+          request.title || '',
+          request.url
+        );
+
+        window.addEventListener('popstate', this.popstate.bind(this), false);
+      } else {
+        window.addEventListener('hashchange', this.hashchange.bind(this), false);
+      }
+
+      return request;
+    }
+
+    hashchange() {
+      const url = window.location.hash.replace('#', '');
+      this.go(url);
+    }
+
+    popstate(event) {
+      const state = event.state;
+
+      if (state && state.routeId) {
+        state.route = router.getRouteById(state.routeId);
+      }
+
+      return state;
+    }
+
+    go(id, meta) {
+      const request = router.build(id, meta);
+
+      if (! request) {
+        throw new Error('fluxapp:router:Go unable to locate route specified', id);
+      }
+
+      if (isHistoryEnabled()) {
+        window.history.pushState(
+          request,
+          request.title,
+          request.url
+        );
+      }
+
+      return request;
+    }
+
+    back() {
+      if (isHistoryEnabled()) {
+        window.history.back();
+      } else {
+        throw new Error('Fluxapp:Router back is not available in hash routing');
+      }
+    }
+
+    forward() {
+      if (isHistoryEnabled()) {
+        window.history.forward();
+      } else {
+        throw new Error('Fluxapp:Router forward is not available in hash routing');
+      }
+    }
+  };
+
   return {
-    [name] : {
-      init(url, meta) {
-        const request = router.build(url, meta, false);
-
-        if (! request) {
-          throw new Error('fluxapp:router:init unable to locate route specified', url);
-        }
-
-        if (isHistoryEnabled()) {
-          window.history.replaceState(
-            request,
-            request.title || '',
-            request.url
-          );
-
-          window.addEventListener('popstate', this.popstate.bind(this), false);
-        } else {
-          window.addEventListener('hashchange', this.hashchange.bind(this), false);
-        }
-
-        return request;
-      },
-
-      hashchange() {
-        const url = window.location.hash.replace('#', '');
-        this.go(url);
-      },
-
-      popstate(event) {
-        const state = event.state;
-
-        if (state && state.routeId) {
-          state.route = router.getRouteById(state.routeId);
-        }
-
-        return state;
-      },
-
-      go(id, meta) {
-        const request = router.build(id, meta);
-
-        if (! request) {
-          throw new Error('fluxapp:router:Go unable to locate route specified', id);
-        }
-
-        if (isHistoryEnabled()) {
-          window.history.pushState(
-            request,
-            request.title,
-            request.url
-          );
-        }
-
-        return request;
-      },
-
-      back() {
-        if (isHistoryEnabled()) {
-          window.history.back();
-        } else {
-          throw new Error('Fluxapp:Router back is not available in hash routing');
-        }
-      },
-
-      forward() {
-        if (isHistoryEnabled()) {
-          window.history.forward();
-        } else {
-          throw new Error('Fluxapp:Router forward is not available in hash routing');
-        }
-      },
-    },
+    [name] : RouterActions,
   };
 };
--- src/stores.js
+++ src/stores.js
@@ -1,114 +1,125 @@
 import Promise from 'bluebird';
-import fluxapp from 'fluxapp';
+import fluxapp, { BaseStore } from 'fluxapp';
 
 const router = fluxapp.getRouter();
 
+function promiseMethod(target, name, descriptor) {
+  descriptor.value = Promise.method(descriptor.value);
+
+  return descriptor;
+}
+
+
 export default (name) => {
+  const RouterStore = class extends BaseStore {
+    static actions = {
+      onInit : `${name}.init`,
+      afterInit : `${name}.init:after`,
+      onGo : `${name}.go`,
+      onPopState : `${name}.popstate`,
+    }
+
+    getInitialState() {
+      return {
+        lastRequest : false,
+      };
+    }
+
+    @promiseMethod
+    _transitionFrom(from, to) {
+      let transition = true;
+
+      if (
+        to && from &&
+        from.routeId !== to.routerId &&
+        from.route.handler &&
+        from.route.handler.willTransitionFrom
+      ) {
+        transition = from.route.handler.willTransitionFrom(to, from);
+      }
+
+      return transition;
+    }
+
+    @promiseMethod
+    _transitionTo(to, transition) {
+      const from = this.getState();
+
+      if (
+        transition !== false &&
+        from.routeId !== to.routeId &&
+        to.route.handler &&
+        to.route.handler.willTransitionTo
+      ) {
+        transition = to.route.handler.willTransitionTo(to, from);
+      }
+
+      return transition;
+    }
+
+    _transition(from, to) {
+      this._transitionFrom(from, to)
+          .then(this._transitionTo.bind(this, to))
+          .then((transition) => {
+            transition = transition !== false;
+
+            if (transition) {
+              to.lastRequest = this.getState();
+
+              this.setState(to);
+            }
+          });
+    }
+
+    onInit(state) {
+      state.route = router.getRouteById(state.routeId);
+
+      this._transitionTo(state).then((transition) => {
+        if (transition !== false) {
+          this.setState(state);
+        }
+      });
+    }
+
+    onGo(result) {
+      result.route = router.getRouteById(result.routeId);
+
+      this._transition(this.getState(), result);
+    }
+
+    onPopState(state) {
+      this._transition(this.getState(), state);
+    }
+
+    isActive(url) {
+      const route = router.getRoute(url);
+      const currentId = this.getState().routeId;
+
+      return route && currentId && route.id === currentId;
+    }
+
+    getLastRequest() {
+      return this.state.lastRequest;
+    }
+
+    getRoute() {
+      return this.state.route;
+    }
+
+    getUrl() {
+      return this.state.url;
+    }
+
+    getParams() {
+      return this.state.params;
+    }
+
+    getQuery() {
+      return this.state.query;
+    }
+  };
+
   return {
-    [name] : {
-      actions : {
-        onInit : 'router.init',
-        afterInit : 'router.init:after',
-        onGo : 'router.go',
-        onPopState : 'router.popstate',
-      },
-
-      getInitialState() {
-        return {
-          lastRequest : false,
-        };
-      },
-
-      _transitionFrom : Promise.method(function _transitionFrom(from, to) {
-        let transition = true;
-
-        if (
-          to && from &&
-          from.routeId !== to.routerId &&
-          from.route.handler &&
-          from.route.handler.willTransitionFrom
-        ) {
-          transition = from.route.handler.willTransitionFrom(to, from);
-        }
-
-        return transition;
-      }),
-
-      _transitionTo : Promise.method(function _transitionTo(to, transition) {
-        const from = this.getState();
-
-        if (
-          transition !== false &&
-          from.routeId !== to.routeId &&
-          to.route.handler &&
-          to.route.handler.willTransitionTo
-        ) {
-          transition = to.route.handler.willTransitionTo(to, from);
-        }
-
-        return transition;
-      }),
-
-      _transition(from, to) {
-        this._transitionFrom(from, to)
-            .then(this._transitionTo.bind(this, to))
-            .then((transition) => {
-              transition = transition !== false;
-
-              if (transition) {
-                to.lastRequest = this.getState();
-
-                this.setState(to);
-              }
-            });
-      },
-
-      onInit(state) {
-        state.route = router.getRouteById(state.routeId);
-
-        this._transitionTo(state).then((transition) => {
-          if (transition !== false) {
-            this.setState(state);
-          }
-        });
-      },
-
-      onGo(result) {
-        result.route = router.getRouteById(result.routeId);
-
-        this._transition(this.getState(), result);
-      },
-
-      onPopState(state) {
-        this._transition(this.getState(), state);
-      },
-
-      isActive(url) {
-        const route = router.getRoute(url);
-        const currentId = this.getState().routeId;
-
-        return route && currentId && route.id === currentId;
-      },
-
-      getLastRequest() {
-        return this.state.lastRequest;
-      },
-
-      getRoute() {
-        return this.state.route;
-      },
-
-      getUrl() {
-        return this.state.url;
-      },
-
-      getParams() {
-        return this.state.params;
-      },
-
-      getQuery() {
-        return this.state.query;
-      },
-    },
+    [name] : RouterStore,
   };
 };
